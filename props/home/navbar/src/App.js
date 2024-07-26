2ce4b656b8250b60e2dd0d360a4cf80baee7bcfa
bhavna <bsoni7698@email.com>
2024-07-26 23:25:23 +0530

    26-7-24

--- props/home/navbar/src/App.js
+++ props/home/navbar/src/App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <Navbar />
+
       <Home />
+      
     </div>
   );
 }
--- props/home/navbar/src/components/Home.js
+++ props/home/navbar/src/components/Home.js
@@ -3,7 +3,9 @@ import "./HomeStyle.css";
 function Home() {
   return (
     <>
-      <div className="Home"></div>
+      <div className="Home">
+        <div className="rn">React Navbar</div>
+      </div>
     </>
   );
 }
